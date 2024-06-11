448c43a41ca8f1aebf5b86f8d56cf254c504ed43
Befæstelser, VD <roadtherma@vd.dk>
2024-06-11 10:02:51 +0200

    Moving average % rounding

--- Roadtherma_app.py
+++ Roadtherma_app.py
@@ -555,7 +555,7 @@ if run_script_checkbox and uploaded_file != None and config['reader'] != None:
     number_2 = np.count_nonzero(moving_average_pixels)
     #ratio 
     ratio = number_2/number_1
-    ratio = np.round(ratio,1)
+    ratio = np.round(ratio*100,1)
     print(ratio*100)
     
     
@@ -563,7 +563,7 @@ if run_script_checkbox and uploaded_file != None and config['reader'] != None:
     st.markdown('Number of pixels detected with moving average method: **{}**'.format(number_2))
     #st.markdown('Ratio af pixels below {}% of moving average temperature: **{}%**'.format(config['moving_average_percent'], np.round(ratio*100,2) ))
     st.markdown('')
-    st.markdown('Ratio af pixels below {}% of moving average temperature: **{}%**'.format(config['moving_average_percent'], ratio*100))
+    st.markdown('Ratio af pixels below {}% of moving average temperature: **{}%**'.format(config['moving_average_percent'], ratio))
     
     #Regner intervaller
     statistics_dataframe = nrn_functions.summary_as_MAP(temperatures_trimmed, road_pixels, moving_average_pixels, filename=st.session_state.uploadFile.name)
