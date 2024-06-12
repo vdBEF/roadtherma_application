e5a07669c1b437ed1595acece69ca227aa8c43f7
Befæstelser, VD <roadtherma@vd.dk>
2024-06-12 10:17:49 +0200

    Temp file test

--- Roadtherma_app.py
+++ Roadtherma_app.py
@@ -278,6 +278,7 @@ if st.session_state.count != st.session_state.count_new:
             
         #st.write(tmp.name)    
         os.remove(tmp.name)
+        print(os.listdir(path='/tmp'))
         #st.write(os.listdir('\tmp'))
         #st.dataframe(load_data(uploaded_file, config['reader']))
         # # remove messages/info if file or reader is removed    
